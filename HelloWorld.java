64572dad522beec9cbe375726deccd899925442d
Abhijeet Srivastava <selvester69@gmail.com>
2021-05-30 03:55:10 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("Hello WOrld in jenkins pipeline build ");
+		System.out.println("Hello WOrld in jenkins pipeline build using webhook ");
 	}
 }
